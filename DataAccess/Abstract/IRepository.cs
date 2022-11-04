1b19f0dae8cdcc5ba3d8943c43dc0b4f6b0ec836
Arseni <123@gmail.bg>
2022-11-04 10:46:03 +0200

    Tuk sazdadoh IRepository Interfeisa

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        T Get(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
