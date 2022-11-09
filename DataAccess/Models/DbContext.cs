2cf416c4f47e95b127907bea43ad72f56c57ce6d
Arseni <123@gmail.bg>
2022-11-09 14:03:17 +0200

    tuk e db contexta

---
+++ DataAccess/Models/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.GeometriesGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class DbContext : DbContext
+    {
+        public DbSet<Positions> Positions { get; set; }
+        public DbSet<Roless> Roles { get; set; }
+        public DbSet<Schedules> Schedules { get; set; }
+        public DbSet<Shifts> Shifts { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<VacationRequests> VacationRequests { get; set; }
+    }
+}
