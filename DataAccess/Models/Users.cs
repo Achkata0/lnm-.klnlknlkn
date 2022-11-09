0189b383e4f84ebe3f61a65398d847cae0d47a28
Achkata0 <ak26826051@edu1.pgzaimov.com>
2022-11-09 12:25:39 +0200

    Tuk sazdadoh klasovete v papkata models

--- DataAccess/Models/Users.cs
+++ DataAccess/Models/Users.cs
@@ -8,5 +8,11 @@ namespace DataAccess.Models
 {
     internal class Users
     {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
     }
 }
